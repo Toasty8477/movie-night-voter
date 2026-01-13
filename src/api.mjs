ff6ce4d18ec5b41dde63458f936ca3e36999b3ab
Alex Horton <alexhorton250@gmail.com>
2026-01-13 14:50:19 -0600

    added license declaration to files

--- src/api.mjs
+++ src/api.mjs
@@ -1,3 +1,9 @@
+/**
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
+**/
+
 import mongoose from "mongoose";
 import express from "express"
 import log from "loglevel"
